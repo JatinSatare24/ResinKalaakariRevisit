75fb2ce60af8f3253dc904bdd166a34b4096c8a8
Jatin Satare <jatinsatare@gmail.com>
2026-05-25 15:04:34 +0530

    add hangleGoogleLogin function to the login component

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import styles from '@/components/Login/Login.module.css'
+import { client } from '@/lib/supabase'
+
+export default function LoginPage() {
+
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [loading, setLoading] = useState<boolean>(false)
+    const [errorMessage, setErrorMessage] = useState<string | null>(null)
+    const [successMessage, setSuccessMessage] = useState<string | null>(null)
+
+    const supabase = client()
+    const router = useRouter()
+
+    const handleGoogleLogin = async () => {
+        try {
+            const { error } = await supabase.auth.signInWithOAuth({
+                provider: 'google',
+                options: {
+                    redirectTo: `${window.location.origin}/auth/callback`
+                },
+            })
+
+            if (error) setErrorMessage(error.message)
+        } catch (err) {
+            console.error('Google login network/execution error:', err)
+            setErrorMessage('An unexpected error occurred while connecting to Google.')
+        }
+    }
+
+    return(
+        <></>
+    )
+}
+
+
