c5e4f071d26d9892c778de09136dc8a04fd5a4f5
Jatin Satare <jatinsatare@gmail.com>
2026-06-17 22:22:43 +0530

    add default homepage route to the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import Link from 'next/link'
+import Hero from '@/components/Hero/Hero'
+import FeaturedProducts from '@/components/FeaturedProducts/FeaturedProducts';
+import ShopByCategory from '@/components/ShopByCategory/ShopByCategory';
+import Gallery from '@/components/Gallery/Gallery';
+import Testimonials from '@/components/Testimonials/Testimonials';
+
 export default function Home() {
-    return (
-        <></>
-    )
+  return (
+    <main>
+      <Hero />
+      <FeaturedProducts />
+      <ShopByCategory />
+      <Gallery />
+      <Testimonials />
+    </main>
+  );
 }
